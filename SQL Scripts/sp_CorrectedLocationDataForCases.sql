043ada90b54d599a0ae50bf3f5c911ef36b1e93a
andya <andyasne@gmail.com>
2023-10-29 00:36:15 +0300

    Added getFamilyCorrections function

---
+++ SQL Scripts/sp_CorrectedLocationDataForCases.sql
@@ -0,0 +1,31 @@
+/*
+   getFamilyCorrections - Retrieve family corrections based on specified criteria
+   
+   This stored procedure retrieves family corrections from the 'family' table. It returns a list of unique case identifiers, owner IDs, and case types for cases where the owner ID matches 'from_location_id' and the village name matches either 'from_village_name1,' 'from_village_name2,' or 'from_village_name3.'
+   
+   Parameters:
+   - to_location_id: The owner ID to be used in the results.
+   - from_location_id: The owner ID to match in the 'family' table.
+   - from_village_name1: The first village name to match.
+   - from_village_name2: The second village name to match.
+   - from_village_name3: The third village name to match.
+   
+   Returns:
+   - A table containing case identifiers, owner IDs, and case types.
+   
+   Usage example:
+   SELECT * FROM getFamilyCorrections('to_location_id_value', 'from_location_id_value', 'village_name1_value', 'village_name2_value', 'village_name3_value');
+*/
+
+CREATE OR REPLACE FUNCTION getFamilyCorrections(to_location_id text, from_location_id text, from_village_name1 text, from_village_name2 text, from_village_name3 text)
+RETURNS TABLE(caseid text, owner_id text, case_type text) AS
+$$
+BEGIN
+  RETURN QUERY
+  SELECT DISTINCT f.caseid, to_location_id AS owner_id, f.case_type
+  FROM family f
+  WHERE f.owner_id = from_location_id
+  AND (f.village_name = from_village_name1 OR f.village_name = from_village_name2 OR f.village_name = from_village_name3);
+END;
+$$
+LANGUAGE 'plpgsql';
--- VB Scripts/Splitbook.vbs
+++ VB Scripts/Splitbook.vbs
@@ -1,13 +1,26 @@
-Sub CopySelectedSheetsToNewWorkbook()
+Sub Splitbook()
+ 
+Dim xPath As String
+xPath = Application.ActiveWorkbook.Path
+Application.ScreenUpdating = False
+Application.DisplayAlerts = False
+For Each xWs In ThisWorkbook.Sheets
+    xWs.Copy
+    Application.ActiveWorkbook.SaveAs Filename:=xPath & "\Training woreda -" & xWs.Name & ".xlsx"
+    Application.ActiveWorkbook.Close False
+Next
+Application.DisplayAlerts = True
+Application.ScreenUpdating = True
+End Sub
+Sub CopySheets()
     Dim sh As Worksheet
     Dim wkbTarget As Workbook
     Dim i, arrNames
     Dim cell As Range, xVRg As Range
-   Set xVRg = Application.InputBox("Please select the column ", "Excel", "", Type:=8)
+   Set xVRg = Application.InputBox("Please select the column that contains the Sheet NAME ", "Excel", "", Type:=8)
     Set wkbTarget = Workbooks.Add()
      Set sh = ThisWorkbook.Sheets("Menus_and_forms")
      sh.Copy After:=wkbTarget.Sheets(1)
-     
     For Each cell In xVRg
       Set sh = Nothing
             On Error Resume Next
@@ -17,6 +30,5 @@ Sub CopySelectedSheetsToNewWorkbook()
                 sh.Copy After:=wkbTarget.Sheets(2)
             End If
     Next cell
-        
       wkbTarget.Sheets("Sheet1").Delete
 End Sub
